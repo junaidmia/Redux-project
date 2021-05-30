9ecf70ed8a6e425a1579cd4e9c1837ad4506d9df
junaidmia <junaidmiabd@gmail.com>
2021-05-30 14:32:24 +0600

    navbar done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Navbar from './Navbar/Navbar';
 
 const Home = () => {
 
-    
+
     return (
+
         <div>
-            
+            <Navbar></Navbar>
         </div>
     );
 };
