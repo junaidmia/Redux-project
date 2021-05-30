a5cbbf91c2f7986b9e89fdd505e7a2b4d8df7857
snapper <zihanjashim2121@gmail.comm>
2021-05-30 23:39:45 +0600

    product detail  added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Home from "./Components/Home/Home";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetails from "./Components/ProductDetails/ProductDetails";
 
 function App() {
   return (
-    <div>
-      <Home> </Home>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Home> </Home>
+        </Route>
+
+        <Route path="/productDetails">
+          <ProductDetails></ProductDetails>
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
